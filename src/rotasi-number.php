958a89df1d0f3832ac5fb37b9b1653d78b53bb16
Rafidimas17 <rafidimas1706@gmail.com>
2024-09-19 07:27:35 +0700

    update:appearance-result

--- src/rotasi-number.php
+++ src/rotasi-number.php
@@ -32,5 +32,6 @@ foreach ($langkah as $langkahTunggal) {
         }
     }, $langkahTunggal, array_keys($langkahTunggal));
 
-    echo implode(' ', $langkahTunggalFormat) . "<br>\n";
+    // echo implode('', $langkahTunggalFormat) . "<br>";
+    echo "<span style='line-height: 1.15;'>" . implode('', $langkahTunggalFormat) . "<br></span>";
 }
